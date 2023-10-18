88b581d579891cfab354f4587531dfa5042767fc
jinchool <qkrwlscnf2@naver.com>
2023-10-18 12:31:30 +0900

    케러셀 이미제작 후 스크립트 적용

--- script/common.js
+++ script/common.js
@@ -1,2 +1,17 @@
 "use strict";
 
+$(() => {
+    //carousel
+    $(".carousel > img:gt(0)").fadeOut(); //투명도를 0으로 하고 display:none을 추가
+
+    function slide() {
+        $(".carousel > img:first")
+            .fadeOut()
+            .next()
+            .fadeIn()
+            .end()
+            .appendTo(".carousel")
+    }
+    
+    let intervalID = setInterval(slide, 3000);
+})
