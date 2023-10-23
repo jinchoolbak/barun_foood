a14a4b3b997541412bd354d8ea61dd87e65ded53
jinchool <qkrwlscnf2@naver.com>
2023-10-23 11:51:20 +0900

    서브 모바일 버전 구조 설계 완료

--- script/sub.js
+++ script/sub.js
@@ -2,7 +2,7 @@ $(() => {
 
     //tabs
     $(".c-title li:nth-child(1)").on("click", (() => {
-        $(".c-body1").css("display", "block")
+        $(".c-body1").css("display", "inline")
         $(".c-body2").css("display", "none")
         $(".c-body3").css("display", "none")
         $(".c-body4").css("display", "none")
@@ -12,7 +12,7 @@ $(() => {
     }))
     $(".c-title li:nth-child(2)").on("click", (() => {
         $(".c-body1").css("display", "none")
-        $(".c-body2").css("display", "block")
+        $(".c-body2").css("display", "inline")
         $(".c-body3").css("display", "none")
         $(".c-body4").css("display", "none")
         $(".c-body5").css("display", "none")
@@ -22,7 +22,7 @@ $(() => {
     $(".c-title li:nth-child(3)").on("click", (() => {
         $(".c-body1").css("display", "none")
         $(".c-body2").css("display", "none")
-        $(".c-body3").css("display", "block")
+        $(".c-body3").css("display", "inline")
         $(".c-body4").css("display", "none")
         $(".c-body5").css("display", "none")
         $(".c-body6").css("display", "none")
@@ -32,7 +32,7 @@ $(() => {
         $(".c-body1").css("display", "none")
         $(".c-body2").css("display", "none")
         $(".c-body3").css("display", "none")
-        $(".c-body4").css("display", "block")
+        $(".c-body4").css("display", "inline")
         $(".c-body5").css("display", "none")
         $(".c-body6").css("display", "none")
         $(".c-body7").css("display", "none")
@@ -42,7 +42,7 @@ $(() => {
         $(".c-body2").css("display", "none")
         $(".c-body3").css("display", "none")
         $(".c-body4").css("display", "none")
-        $(".c-body5").css("display", "block")
+        $(".c-body5").css("display", "inline")
         $(".c-body6").css("display", "none")
         $(".c-body7").css("display", "none")
     }))
@@ -52,7 +52,7 @@ $(() => {
         $(".c-body3").css("display", "none")
         $(".c-body4").css("display", "none")
         $(".c-body5").css("display", "none")
-        $(".c-body6").css("display", "block")
+        $(".c-body6").css("display", "inline")
         $(".c-body7").css("display", "none")
     }))
     $(".c-title li:nth-child(7)").on("click", (() => {
@@ -62,6 +62,6 @@ $(() => {
         $(".c-body4").css("display", "none")
         $(".c-body5").css("display", "none")
         $(".c-body6").css("display", "none")
-        $(".c-body7").css("display", "block")
+        $(".c-body7").css("display", "inline")
     }))
 });
